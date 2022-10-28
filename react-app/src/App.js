97bf7071d73fe62f2dd8fa4a9e0de0880cfe86d8
Rupesh Khadka <rupesss09@gmail.com>
2022-10-28 12:08:19 -0400

    React project first calculator operation completed.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      x: 10,
+      y: 5,
+      ans: 0,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Calc Ops</h1>
+        <p>x = {this.state.x}</p>
+        <p>y = {this.state.y}</p>
+        <h3>Ans is: {this.state.ans}</h3>
+        <hr></hr>
+        <button
+          onClick={() => {
+            this.calc("ADD");
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          ADD
+        </button>
+        <button
+          onClick={() => {
+            this.calc("SUB");
+          }}
+        >
+          SUB
+        </button>
+        <button
+          onClick={() => {
+            this.calc("MUL");
+          }}
+        >
+          MUL
+        </button>
+        <button
+          onClick={() => {
+            this.calc("DIV");
+          }}
+        >
+          DIV
+        </button>
+      </div>
+    );
+  }
 
-export default App;
+  calc(op) {
+    console.log(op);
+    switch (op) {
+      case "ADD":
+        this.setState({
+          ans: this.state.x + this.state.y,
+        });
+        break;
+      case "SUB":
+        this.setState({
+          ans: this.state.x - this.state.y,
+        });
+        break;
+      case "MUL":
+        this.setState({
+          ans: this.state.x * this.state.y,
+        });
+        break;
+      case "DIV":
+        this.setState({
+          ans: this.state.x / this.state.y,
+        });
+        break;
+      default:
+    }
+  }
+}
